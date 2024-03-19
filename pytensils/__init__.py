efeb27acc4f7c3f5a84feb4029beeeea8ac33656
Thomas Eleff <tome18@comcast.net>
2024-03-19 17:39:53 -0500

    Fix to setup.cfg

--- pytensils/__init__.py
+++ pytensils/__init__.py
@@ -1,5 +1,6 @@
-import pytensils.config
-import pytensils.utils
-import pytensils.profiler
+import pytensils.config as config
+import pytensils.utils as utils
+import pytensils.profiler as profiler
 
+__name__ = 'pytensils'
 __all__ = ['config', 'utils', 'profiler']
